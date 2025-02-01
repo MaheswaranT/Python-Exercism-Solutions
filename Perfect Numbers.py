eb4b5b8859d537e3564231064419579719343f8a
Maheswaran <maheshwaranm75@gmail.com>
2025-02-01 09:55:58 +0530

    Create Perfect Numbers.py

---
+++ Perfect Numbers.py
@@ -0,0 +1,12 @@
+def classify(number):
+    if number < 1:
+        raise ValueError("Classification is only possible for positive integers.")
+    
+    aliquot_sum = sum([i for i in range(1, number) if number % i == 0])
+    
+    if aliquot_sum == number:
+        return "perfect"
+    elif aliquot_sum > number:
+        return "abundant"
+    else:
+        return "deficient"
